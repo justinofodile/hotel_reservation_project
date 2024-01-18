4be45f4b95650ff6b897825f9cb4cba38fe875d7
Ofodile Justin <justinofodile@gmail.com>
2024-01-18 18:12:24 +0100

    update on client

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ mongoose.connection.on("disconnected", () => {
 
 //middlewares
 app.use(cors({
-  origin: ["https://hotel-reservation-project-eight.vercel.app", "https://hotel-reservation-project-cpa1.vercel.app/"],
+  origin: ["https://hotel-reservation-project-eight.vercel.app"],
   // origin: ["http://localhost:3000", "https://hotel-reservation-project-eight.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true,
